3d93ce22c68d7e15343126915a5f0a630273802e
peeweemiwie <miwayagi@gmail.com>
2021-12-04 19:38:10 -0500

    Add router

--- src/components/Dictionary.js
+++ src/components/Dictionary.js
@@ -2,6 +2,7 @@ import useAxios from './useAxios';
 import WordOrigin from './WordOrigin';
 import Phonetics from './Phonetics';
 import Meanings from './Meanings';
+import Images from './Images';
 import './Dictionary.scss';
 
 const Dictionary = ({ keyword, handleClickedWord }) => {
@@ -13,17 +14,20 @@ const Dictionary = ({ keyword, handleClickedWord }) => {
 			{isPending && <p>Loading...</p>}
 			{error && <p>Sorry... Could not find from the resource</p>}
 			{!isPending && !error && (
-				<div className='response-data'>
-					<div className='card'>
-						<WordOrigin word={data.data[0].word} origin={data.data[0].origin} />
-						<Phonetics data={data.data[0].phonetics} />
-					</div>
-					<Meanings
-						meanings={data.data[0].meanings}
-						handleClickedWord={handleClickedWord}
-					/>
+				<div className='card'>
+					<WordOrigin word={data.data[0].word} origin={data.data[0].origin} />
+					<Phonetics data={data.data[0].phonetics} />
 				</div>
 			)}
+
+			<nav>nav goes here! </nav>
+			{!isPending && !error && (
+				<Meanings
+					meanings={data.data[0].meanings}
+					handleClickedWord={handleClickedWord}
+				/>
+			)}
+			<Images keyword={keyword} />
 		</div>
 	);
 };
